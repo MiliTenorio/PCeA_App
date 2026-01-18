ab810fb8e599d34f3b1780b0bc1dcb08465a5a07
MiliTenorio <milena.rtenorio@gmail.com>
2026-01-18 13:53:55 -0400

    fix: remove unnecassary entity

--- pcea_app/lib/features/voluntario/data/datasources/voluntario_remote_data_source.dart
+++
@@ -1,9 +0,0 @@
-import '../models/voluntario_model.dart';
-
-abstract class VoluntarioRemoteDataSource {
-  Future<List<VoluntarioModel>> getAllVoluntarios();
-  Future<VoluntarioModel> getVoluntarioById(int id);
-  Future<VoluntarioModel> createVoluntario(VoluntarioModel voluntario);
-  Future<VoluntarioModel> updateVoluntario(VoluntarioModel voluntario);
-  Future<void> deleteVoluntario(int id);
-}
--- pcea_app/lib/features/voluntario/domain/repositories/voluntario_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/voluntario.dart';
-
-abstract class VoluntarioRepository {
-  Future<List<Voluntario>> getAllVoluntarios();
-  Future<Voluntario> getVoluntarioById(int id);
-  Future<Voluntario> createVoluntario(Voluntario voluntario);
-  Future<Voluntario> updateVoluntario(Voluntario voluntario);
-  Future<void> deleteVoluntario(int id);
-}
